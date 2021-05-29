9a314d7f1811ec36adbbc2f84ff4b518c81c41c4
vikrant <vikrantthakur15@gmail.com>
2021-05-29 17:48:03 +0530

    changes added

--- js/script-lib.js
+++ js/script-lib.js
@@ -1,4 +1,10 @@
 var serverURL="https://6azqonbehd.execute-api.ap-south-1.amazonaws.com/dev/rajpurohit/"
+checkHttpReq()
+function checkHttpReq(){	
+	if (location.protocol == 'http:'){
+	  location.href = location.href.replace("http","https");
+	}
+}
 function saveCustData(obj){
 
 
@@ -89,8 +95,14 @@ function oTPSend(mobile,obj){
 function verifyOTPandSave(obj,otp,mobile){
 	$(obj).attr('onclick', "");
 	$(obj).html('Please Wait....<i class="fa fa-angle-right" aria-hidden="true"></i>');
+	
+	var custPic="";	
+	if($("#custPic-image").attr('src') != undefined && $("#custPic-image").attr('src') != ""){
+		custPic = $("#custPic-image").attr('src').split(',')[1];
+	}
 	var map = getFormData($("#formDataid"));
 	map["otp"]=otp;
+	map["custPic"]=custPic;
 	$.ajax({
 			type: 'POST',
 			data: JSON.stringify(map),
@@ -102,8 +114,7 @@ function verifyOTPandSave(obj,otp,mobile){
 				}else{
 					alert(response);
 					location.reload();
-				}
-				
+				}				
 			},
 			error: function (response) {
 				alert("Error while Login "+response);
@@ -136,12 +147,13 @@ function fetchAllCustInfo(){
 function getCustDetails(obj){
 	var map={};
 	map["id"]=$(obj).attr('data-id');
-	
+	fetchImageDetails($(obj).attr('data-id'),'image-sec');
 	$.ajax({
 		type: 'POST',
 		data: JSON.stringify(map),
 		url: serverURL + "raj_fetchCustDetails",
 		success: function (response) {
+			
 			console.log(response);
 			for (var key in response) {
 			  if (response.hasOwnProperty(key)) {
@@ -170,3 +182,56 @@ function getMap(){
 		}
 	});	
 }
+
+   function fetchImageDetails(custid,divClass){
+		$("."+divClass+"-cust").append('<i class="fa fa-refresh fa-spin " style="font-size: 81px;color: #2e6da4;"></i>');
+		$("."+divClass+"-cust").find('img').attr('src','data:image/png;base64,');
+		var map= {};
+		map["id"]=custid;
+        $.ajax({
+            type: "POST",
+            url: serverURL +"raj_downloadPic",
+            data: JSON.stringify(map),
+            success: function (response) {
+				$("."+divClass+"-cust").find('.fa-refresh').remove();
+                $("."+divClass+"-cust").find('img').attr('src','data:image/png;base64,'+response);  
+            },
+            error: function (e) {
+                alert(e);
+            }
+        });
+    }
+
+function resizeCustPic(){
+		resizeImageToSpecificWidth("custPic",input2);
+}
+function resizeImageToSpecificWidth(imgPath,myInput) {
+		var width = 200;
+        if (myInput.files && myInput.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(event) {
+                var img = new Image();
+                img.onload = function() {
+                    if (img.width > width) {
+                        var oc = document.createElement('canvas'), octx = oc.getContext('2d');
+                        oc.width = img.width;
+                        oc.height = img.height;
+                        octx.drawImage(img, 0, 0);
+                        while (oc.width * 0.5 > width) {
+                            oc.width *= 0.5;
+                            oc.height *= 0.5;
+                            octx.drawImage(oc, 0, 0, oc.width, oc.height);
+                        }
+                        oc.width = width;
+                        oc.height = oc.width * img.height / img.width;
+                        octx.drawImage(img, 0, 0, oc.width, oc.height);
+						$(".fa-refresh").remove();
+                        document.getElementById(imgPath+"-image").src = oc.toDataURL();
+                    }
+                };
+                document.getElementById(imgPath+"-orignal").src = event.target.result;
+                img.src = event.target.result;
+            };
+            reader.readAsDataURL(myInput.files[0]);
+        }
+    }
